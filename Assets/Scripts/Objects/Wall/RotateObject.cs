55e9c79f16c43980f80d11936e380a1daa0db774
Olivier <olipc97@hotmail.com>
2016-10-08 20:42:47 -0400

    j'ai fait sa

---
+++ Assets/Scripts/Objects/Wall/RotateObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 1000 * Time.deltaTime));
+    }
+}
